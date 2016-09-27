ef11e6da2c1e93d45244d5cbcde81db31da26887
JMauris <justine.mauris@students.hevs.ch>
2016-09-27 11:29:37 +0200

    delet

--- www/cas/include/logout.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include('../include/toppage.inc');
-    $_SESSION['nom'] = '';
-    $_SESSION['prenom'] = '';
-?>
-
-<script>javascript:window.location='../mobile/connexion.php';</script>
--- www/php_mvc/views/error/http404.php
+++
@@ -1,7 +0,0 @@
-<?php include_once  '/views/header.inc'; ?>
-<font size="+1"><strong><?php echo $this->vars['title']?></strong></font><hr/>
-<?php
-
-echo 'Page '.$this->vars['controller'].'/'. $this->vars['method']. ' '. 'not found';
-
-include_once '/views/footer.inc';
